5fcdc54483d9aff98512e05cce60f29522bc3aaf
bawankarneha <87469699+bawankarneha@users.noreply.github.com>
2021-07-26 22:33:49 +0530

    Add files via upload

---
+++ authapi/controllers/change-controllers.js
@@ -0,0 +1,23 @@
+var connection = require('./config');
+var bcrypt = require('bcrypt');
+
+module.exports.change=function(req,res){
+var email = req.body.email;
+var password = req.body.password;
+
+connection.query('UPDATE users SET password =? WHERE email =?',[password,email],function (error, results, fields) {
+            if(error){
+                res.json({
+                status:false,
+                message:error
+               })
+            }
+            else{
+                res.json({
+                status:true,
+                message:"Password Updated"
+                })
+            }
+        
+    });    
+}
