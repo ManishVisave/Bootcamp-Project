e630d590fe8397254b046aebf73cdbaba5273997
kunal-sachdev <kunal.sachdev@acquia.com>
2021-07-26 18:21:31 +0530

    Users table added in property database

--- dumps/property.sql
+++ dumps/property.sql
@@ -159,8 +159,10 @@ CREATE TABLE `property` (
   PRIMARY KEY (`property_id`),
   KEY `currency_id` (`currency_id`),
   KEY `location_id` (`location_id`),
+  KEY `user_id` (`user_id`),
   CONSTRAINT `property_ibfk_3` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`currency_id`) ON DELETE CASCADE,
-  CONSTRAINT `property_ibfk_4` FOREIGN KEY (`location_id`) REFERENCES `locality` (`location_id`) ON DELETE CASCADE
+  CONSTRAINT `property_ibfk_4` FOREIGN KEY (`location_id`) REFERENCES `locality` (`location_id`) ON DELETE CASCADE,
+  CONSTRAINT `property_ibfk_5` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -172,6 +174,33 @@ LOCK TABLES `property` WRITE;
 /*!40000 ALTER TABLE `property` DISABLE KEYS */;
 /*!40000 ALTER TABLE `property` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `whatsapp` varchar(10) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -182,4 +211,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-26  5:41:25
+-- Dump completed on 2021-07-26 12:48:52
