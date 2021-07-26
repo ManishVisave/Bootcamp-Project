dc69a3e114df697495865629be90e22cbe397e73
bawankarneha <87469699+bawankarneha@users.noreply.github.com>
2021-07-26 22:37:10 +0530

    Update authenticate-controller.js

--- authapi/controllers/authenticate-controller.js
+++ authapi/controllers/authenticate-controller.js
@@ -1,4 +1,6 @@
 var connection = require('./config');
+var bcrypt = require("bcryptjs");
+var jwt=require('jsonwebtoken');
 
 module.exports.authenticate=function(req,res){
     var email=req.body.email;
@@ -12,19 +14,26 @@ module.exports.authenticate=function(req,res){
             })
       }else{
         if(results.length >0){
-            if(password==results[0].password){
-                res.json({
-                    status:true,
-                    message:'successfully authenticated'
-                })
-            }else{
-                res.json({
-                  status:false,
-                  message:"Email and password does not match"
-                 });
-            }
-         
-        }
+          bcrypt.compare(password, results[0].password, function(err, ress) {
+            
+              if(!ress){
+                  res.json({
+                    status:false,                  
+                    message:err,
+                    message:"Email and password does not match"
+                  });
+              }else{           
+                var token=jwt.sign(results[0],process.env.SECRET_KEY,{
+                  expiresIn:5000
+              });         
+                  res.json({
+                      status:true,
+                      token :token,
+                      
+                  })
+              }
+          });         
+      }
         else{
           res.json({
               status:false,    
