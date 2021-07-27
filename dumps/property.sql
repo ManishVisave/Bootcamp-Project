f62fbb93258be12c51479345ea99a09b35b74f21
manish visave <manish.visave@acquia.com>
2021-07-27 20:27:48 +0530

    Database Updated

--- dumps/property.sql
+++ dumps/property.sql
@@ -19,10 +19,6 @@
 -- Table structure for table `city`
 --
 
-CREATE USER 'test'@'%' IDENTIFIED WITH mysql_native_password BY 'test@123';
-GRANT ALL ON *.* to 'test'@'%';
-USE property;
-
 DROP TABLE IF EXISTS `city`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -33,7 +29,7 @@ CREATE TABLE `city` (
   PRIMARY KEY (`city_id`),
   KEY `country_id` (`country_id`),
   CONSTRAINT `city_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,6 +38,7 @@ CREATE TABLE `city` (
 
 LOCK TABLES `city` WRITE;
 /*!40000 ALTER TABLE `city` DISABLE KEYS */;
+INSERT INTO `city` VALUES (1,1,'Pune'),(2,3,'Mumbai');
 /*!40000 ALTER TABLE `city` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,7 +53,7 @@ CREATE TABLE `country` (
   `country_id` int NOT NULL AUTO_INCREMENT,
   `country_name` varchar(255) NOT NULL,
   PRIMARY KEY (`country_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,6 +62,7 @@ CREATE TABLE `country` (
 
 LOCK TABLES `country` WRITE;
 /*!40000 ALTER TABLE `country` DISABLE KEYS */;
+INSERT INTO `country` VALUES (1,'I'),(2,'I'),(3,'India'),(4,'U'),(5,'U');
 /*!40000 ALTER TABLE `country` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,7 +77,7 @@ CREATE TABLE `currency` (
   `currency_id` int NOT NULL AUTO_INCREMENT,
   `currency_name` varchar(255) NOT NULL,
   PRIMARY KEY (`currency_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,6 +86,7 @@ CREATE TABLE `currency` (
 
 LOCK TABLES `currency` WRITE;
 /*!40000 ALTER TABLE `currency` DISABLE KEYS */;
+INSERT INTO `currency` VALUES (1,'INR');
 /*!40000 ALTER TABLE `currency` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,7 +105,7 @@ CREATE TABLE `locality` (
   PRIMARY KEY (`location_id`),
   KEY `city_id` (`city_id`),
   CONSTRAINT `locality_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,6 +114,7 @@ CREATE TABLE `locality` (
 
 LOCK TABLES `locality` WRITE;
 /*!40000 ALTER TABLE `locality` DISABLE KEYS */;
+INSERT INTO `locality` VALUES (1,1,'Dhayari',411041),(2,1,'ABC1',411031),(3,1,'ABC2',411021),(4,2,'ABC3',400031);
 /*!40000 ALTER TABLE `locality` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -142,48 +142,6 @@ LOCK TABLES `photo` WRITE;
 /*!40000 ALTER TABLE `photo` ENABLE KEYS */;
 UNLOCK TABLES;
 
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `mobile` varchar(10) NOT NULL,
-  `whatsapp` varchar(10) NOT NULL,
-  `location_id` int NOT NULL,
-  PRIMARY KEY (`user_id`),
-  KEY `location_id` (`location_id`),
-  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `locality` (`location_id`) ON DELETE CASCADE
-  
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
-
 --
 -- Table structure for table `property`
 --
@@ -209,7 +167,7 @@ CREATE TABLE `property` (
   CONSTRAINT `property_ibfk_3` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`currency_id`) ON DELETE CASCADE,
   CONSTRAINT `property_ibfk_4` FOREIGN KEY (`location_id`) REFERENCES `locality` (`location_id`) ON DELETE CASCADE,
   CONSTRAINT `property_ibfk_5` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -218,10 +176,10 @@ CREATE TABLE `property` (
 
 LOCK TABLES `property` WRITE;
 /*!40000 ALTER TABLE `property` DISABLE KEYS */;
+INSERT INTO `property` VALUES (13,1,'Dhayari',1,334.00,334.00,111556.00,4000000.00,'Nice',1),(14,1,'Dhayari',1,334.00,334.00,111556.00,4000000.00,'Nice',1),(15,2,'sr00',1,334.00,334.00,111556.00,4000000.00,'Nice',1),(16,3,'sr01',1,334.00,334.00,111556.00,4000000.00,'Nice',1),(17,3,'sr01',1,225.00,225.00,50625.00,4000000.00,'Nice',1),(18,4,'sr02',1,225.00,225.00,50625.00,4000000.00,'Nice',1),(19,4,'sr02',1,225.00,225.00,50625.00,4000000.00,'Nice',1);
 /*!40000 ALTER TABLE `property` ENABLE KEYS */;
 UNLOCK TABLES;
 
-<<<<<<< HEAD
 --
 -- Table structure for table `users`
 --
@@ -238,7 +196,7 @@ CREATE TABLE `users` (
   `whatsapp` varchar(10) NOT NULL,
   `location` varchar(255) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -247,6 +205,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'neha','neha@b.com','$2a$08$ElMQl/o1pLRfKia3E8vpDO4K4DRvusr1.KeNVphV.jug0.mCWVcsG','1234567889','1233456789','nagpur');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -259,7 +218,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-26 16:35:13
-=======
--- Dump completed on 2021-07-26 12:48:52
->>>>>>> 68620c3fdb8427a6c367f6e7c81f3c8d01a4ea51
+-- Dump completed on 2021-07-27 14:55:54
