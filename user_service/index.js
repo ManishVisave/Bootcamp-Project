5858c9ed990f7121406e650cc50eec6e25d5fc8a
bawankarneha <87469699+bawankarneha@users.noreply.github.com>
2021-07-27 23:47:31 +0530

    Update index.js

--- user_service/index.js
+++ user_service/index.js
@@ -1,49 +1,26 @@
 const express=require("express");
-var jwt= require("jsonwebtoken");
-
 const app = express();
-var router=express.Router();
-
+process.env.SECRET_KEY="thisismysecretkey";
 //parse request of content type - application/json
 app.use(express.json());
 
 //parse request content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({extended:true}));
 
+const{ checkToken } = require("./auth/token_validation");
+
 var authenticateController=require('./authenticate-controller');
 var registerController=require('./register-controller');
 var deleteController = require('./delete-controller');
-var changeController = require('./change-controllers');
+var changeController = require('./change-controller');
+var detailsController = require('./details-controller');
 
-process.env.SECRET_KEY="thisismysecretkey";
-
-
-//route to handle login and registration 
+//route to handle login ,registration , delete user and change password  
 app.post('/api/register',registerController.register);
 app.post('/api/authenticate',authenticateController.authenticate);
-app.post('/secure-api/delete',deleteController.delete);
-app.post('/secure-api/change',changeController.change);
-
-app.use('/secure-api',router);
-// validation middleware
-router.use(function(req,res,next){
-    var token=req.body.token || req.headers['token'];
-    if(token){
-        jwt.verify(token,process.env.SECRET_KEY,function(err,ress){
-            if(err){
-                res.status(500).send('Token Invalid');
-            }else{
-                next();
-            }
-        })
-    }else{
-        res.send('Please send a token')
-    }
-})
-router.get('/home',function(req,res){
-    res.send('Token Verified')
-})
-
+app.post('/api/delete',checkToken,deleteController.delete);
+app.post('/api/change',checkToken,changeController.change);
+app.get('/api/details',checkToken,detailsController.details);
 
 app.listen(8081, () =>console.log('Server Running on 8081'));
 
