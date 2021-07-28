0ea073bf6060beeb0ac363ef46c98961555da561
manish visave <manish.visave@acquia.com>
2021-07-28 10:43:23 +0530

    Hiding Credentials

--- user_service/change-controllers.js
+++ user_service/change-controllers.js
@@ -1,5 +1,5 @@
-ar connection = require('./config');
-var bcrypt = require('bcrypt');
+var connection = require('./config');
+var bcrypt = require('bcryptjs');
 
 module.exports.change=function(req,res){
 var email = req.body.email;
